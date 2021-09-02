49edb7dcf1c12c881890d1ff16d756021e9dbe0f
Artem Kolakovskyi <kolakovskyiartem@gmail.com>
2021-09-02 16:38:32 +0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,36 @@
+
+//1) с помощью промта спрашиваем у пользователя что он хочет сделать(add, sub, mult, div).
+//2) с помощью промта спрашиваем у пользователя первое число.
+//3) с помощью промта спрашиваем у пользователя второе число.
+//4) С помощью alert или console.log выводим результат действия(add, sub, mult, div) со всеми операндами(Н - р "2 + 3 = 5")
+// let askUser = prompt('Введите кодовое слово (add, sub, mult, div), которое будет выполнять математическую операцию двух вами заданных чисел: (add = сложению, sub = вычитанию, mult = умножению, div = делению)');
+// let number1 = +prompt('Введите первое число');
+// let number2 = +prompt('Введите второе число');
+
+
+
+// if (askUser === 'add') {
+//     let result1 = number1 + number2;
+//     alert(`${number1} + ${number2} = ${result1}`);  
+// }
+// else if (askUser ==='sub') {
+//     let result2 = number1 - number2;
+//     alert(`${number1} - ${number2} = ${result2}`);
+// } else if (askUser==='mult') {
+//     let result3 = number1 * number2;
+//     alert(`${number1} * ${number2} = ${result3}`);
+// }
+// else if (askUser ==='div') {
+//     let result4 = number1 / number2;
+//     alert(`${number1} / ${number2} = ${result4}`);
+// } else {
+//     alert('Вы ввели неверное кодовое слово');
+//     }
+
+let age = +prompt('Hello');
+
+let user = {
+    name: 'Hello',
+    age,
+    24: 34,
+}
